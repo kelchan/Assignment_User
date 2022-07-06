c572bb9290b94ff83843e70eece8dbe796f75309
kelchan <kelvin.chan131@gmail.com>
2022-07-05 21:37:08 -0700

    first commit

---
+++ user.py
@@ -0,0 +1,52 @@
+class User:
+    def __init__(self, first_name, last_name, email, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.is_rewards_member = False
+        self.gold_card_points = 0
+
+    def display_info(self):
+        info = [
+            self.first_name,
+            self.last_name,
+            self.email,
+            "Age - " + str(self.age),
+            "Reward Member - " + str(self.is_rewards_member),
+            "Gold Points - " + str(self.gold_card_points)
+        ]
+        print(info)
+
+    def enroll(self):
+        if self.is_rewards_member == True:
+            print( "Is already a member!" )
+        self.is_rewards_member = True
+        self.gold_card_points = 200
+
+    def spend_points(self, amount):
+        if self.gold_card_points < amount:
+            print( "Not enough points!" )
+        elif self.gold_card_points >= amount:
+            self.gold_card_points = self.gold_card_points - amount
+
+Kelvin = User("Kelvin", "Chan", "kelvin.chan131@gmail.com", 23)
+Kelvin.display_info()
+Kelvin.enroll()
+Kelvin.display_info()
+Kelvin.spend_points(50)
+Kelvin.display_info()
+
+User2 = User("Jane", "Doe", "janedoe@gmail.com", 24)
+User2.display_info()
+User2.enroll()
+User2.display_info()
+User2.spend_points(80)
+
+Kelvin.display_info(), User2.display_info()
+
+Kelvin.enroll()
+
+User3 = User("Billy", "Bob", "billybob@gmail.com", 25)
+User3.display_info()
+User3.spend_points(40)
